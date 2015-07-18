affc5ce1fb71471f5e945e75af41e992f4dfcbe1
David Rogers <davidrogersau@yahoo.com.au>
2015-07-18 17:38:25 +0930

    Did some further wiring up of the Person view and managing resource disposal.

--- KesselRun.HomeLibrary.Ui/Forms/MainForm.Designer.cs
+++ KesselRun.HomeLibrary.Ui/Forms/MainForm.Designer.cs
@@ -26,21 +26,21 @@ namespace KesselRun.HomeLibrary.Ui.Forms
             this.btnLendings = new System.Windows.Forms.Button();
             this.lstMainViewLog = new System.Windows.Forms.ListBox();
             this.btnClose = new System.Windows.Forms.Button();
-            this.logDisplayBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.pnlTopArea = new System.Windows.Forms.Panel();
             this.lendingsSearchCriteriaControl1 = new KesselRun.HomeLibrary.Ui.UserControls.LendingsSearchCriteriaControl();
+            this.logDisplayBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.logDisplayBindingSource)).BeginInit();
             this.pnlTopArea.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.logDisplayBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // MainContentPanel
             // 
             this.MainContentPanel.BackColor = System.Drawing.SystemColors.Menu;
-            this.MainContentPanel.Location = new System.Drawing.Point(12, 233);
-            this.MainContentPanel.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.MainContentPanel.Location = new System.Drawing.Point(12, 229);
+            this.MainContentPanel.Margin = new System.Windows.Forms.Padding(6);
             this.MainContentPanel.Name = "MainContentPanel";
-            this.MainContentPanel.Size = new System.Drawing.Size(2140, 508);
+            this.MainContentPanel.Size = new System.Drawing.Size(2140, 576);
             this.MainContentPanel.TabIndex = 0;
             // 
             // panel1
@@ -49,7 +49,7 @@ namespace KesselRun.HomeLibrary.Ui.Forms
             this.panel1.Controls.Add(this.btnBooks);
             this.panel1.Controls.Add(this.btnLendings);
             this.panel1.Location = new System.Drawing.Point(8, 25);
-            this.panel1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.panel1.Margin = new System.Windows.Forms.Padding(6);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(268, 192);
             this.panel1.TabIndex = 1;
@@ -57,7 +57,7 @@ namespace KesselRun.HomeLibrary.Ui.Forms
             // btnPeople
             // 
             this.btnPeople.Location = new System.Drawing.Point(16, 125);
-            this.btnPeople.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.btnPeople.Margin = new System.Windows.Forms.Padding(6);
             this.btnPeople.Name = "btnPeople";
             this.btnPeople.Size = new System.Drawing.Size(236, 44);
             this.btnPeople.TabIndex = 2;
@@ -68,7 +68,7 @@ namespace KesselRun.HomeLibrary.Ui.Forms
             // btnBooks
             // 
             this.btnBooks.Location = new System.Drawing.Point(16, 69);
-            this.btnBooks.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.btnBooks.Margin = new System.Windows.Forms.Padding(6);
             this.btnBooks.Name = "btnBooks";
             this.btnBooks.Size = new System.Drawing.Size(236, 44);
             this.btnBooks.TabIndex = 1;
@@ -79,27 +79,28 @@ namespace KesselRun.HomeLibrary.Ui.Forms
             // btnLendings
             // 
             this.btnLendings.Location = new System.Drawing.Point(16, 19);
-            this.btnLendings.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.btnLendings.Margin = new System.Windows.Forms.Padding(6);
             this.btnLendings.Name = "btnLendings";
             this.btnLendings.Size = new System.Drawing.Size(236, 44);
             this.btnLendings.TabIndex = 0;
             this.btnLendings.Text = "LendingsViewModel";
             this.btnLendings.UseVisualStyleBackColor = true;
+            this.btnLendings.Click += new System.EventHandler(this.btnLendings_Click);
             // 
             // lstMainViewLog
             // 
             this.lstMainViewLog.FormattingEnabled = true;
             this.lstMainViewLog.ItemHeight = 25;
-            this.lstMainViewLog.Location = new System.Drawing.Point(12, 754);
-            this.lstMainViewLog.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.lstMainViewLog.Location = new System.Drawing.Point(12, 824);
+            this.lstMainViewLog.Margin = new System.Windows.Forms.Padding(6);
             this.lstMainViewLog.Name = "lstMainViewLog";
             this.lstMainViewLog.Size = new System.Drawing.Size(2134, 179);
             this.lstMainViewLog.TabIndex = 2;
             // 
             // btnClose
             // 
-            this.btnClose.Location = new System.Drawing.Point(2002, 948);
-            this.btnClose.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.btnClose.Location = new System.Drawing.Point(2002, 1018);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(6);
             this.btnClose.Name = "btnClose";
             this.btnClose.Size = new System.Drawing.Size(150, 44);
             this.btnClose.TabIndex = 4;
@@ -107,15 +108,11 @@ namespace KesselRun.HomeLibrary.Ui.Forms
             this.btnClose.UseVisualStyleBackColor = true;
             this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
             // 
-            // logDisplayBindingSource
-            // 
-            this.logDisplayBindingSource.DataSource = typeof(KesselRun.HomeLibrary.UiModel.ViewModels.MainViewModel);
-            // 
             // pnlTopArea
             // 
             this.pnlTopArea.Controls.Add(this.lendingsSearchCriteriaControl1);
             this.pnlTopArea.Location = new System.Drawing.Point(288, 25);
-            this.pnlTopArea.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.pnlTopArea.Margin = new System.Windows.Forms.Padding(6);
             this.pnlTopArea.Name = "pnlTopArea";
             this.pnlTopArea.Size = new System.Drawing.Size(1862, 192);
             this.pnlTopArea.TabIndex = 5;
@@ -129,22 +126,26 @@ namespace KesselRun.HomeLibrary.Ui.Forms
             this.lendingsSearchCriteriaControl1.Size = new System.Drawing.Size(1000, 173);
             this.lendingsSearchCriteriaControl1.TabIndex = 0;
             // 
+            // logDisplayBindingSource
+            // 
+            this.logDisplayBindingSource.DataSource = typeof(KesselRun.HomeLibrary.UiModel.ViewModels.MainViewModel);
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(2168, 1010);
+            this.ClientSize = new System.Drawing.Size(2174, 1079);
             this.Controls.Add(this.pnlTopArea);
             this.Controls.Add(this.btnClose);
             this.Controls.Add(this.lstMainViewLog);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.MainContentPanel);
-            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "MainForm";
             this.Text = "Home Library";
             this.panel1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.logDisplayBindingSource)).EndInit();
             this.pnlTopArea.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.logDisplayBindingSource)).EndInit();
             this.ResumeLayout(false);
 
         }
--- KesselRun.HomeLibrary.UiLogic/Presenters/PeoplePresenter.cs
+++ KesselRun.HomeLibrary.UiLogic/Presenters/PeoplePresenter.cs
@@ -1,10 +1,12 @@
 ﻿using KesselRun.HomeLibrary.UiLogic.Views;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using WinFormsMvp;
+using WinFormsMvp.Binder;
 
 namespace KesselRun.HomeLibrary.UiLogic.Presenters
 {
@@ -14,12 +16,17 @@ namespace KesselRun.HomeLibrary.UiLogic.Presenters
         public PeoplePresenter(IPeopleView view)
             : base(view)
         {
+            View.ViewClosing += View_ViewClosing;
+        }
 
+        void View_ViewClosing(object sender, System.EventArgs e)
+        {
+            PresenterBinder.Factory.Release(this);
         }
 
         public void Dispose()
         {
-            throw new NotImplementedException();
+            Debug.WriteLine("HIT");
         }
     }
 }
