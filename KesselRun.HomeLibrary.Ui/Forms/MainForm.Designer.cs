a4e10cc8208bf51127b0506286e4657819040bf1
David Rogers <davidrogersau@yahoo.com.au>
2016-01-10 16:50:38 +1030

    Test for Github key.

--- KesselRun.HomeLibrary.Ui/Forms/MainForm.Designer.cs
+++ KesselRun.HomeLibrary.Ui/Forms/MainForm.Designer.cs
@@ -25,8 +25,8 @@
             this.lstMainViewLog = new System.Windows.Forms.ListBox();
             this.btnClose = new System.Windows.Forms.Button();
             this.pnlTopArea = new System.Windows.Forms.Panel();
-            this.lendingsSearchCriteriaControl1 = new KesselRun.HomeLibrary.Ui.UserControls.LendingsSearchCriteriaControl();
             this.logDisplayBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.lendingsSearchCriteriaControl1 = new KesselRun.HomeLibrary.Ui.UserControls.LendingsSearchCriteriaControl();
             this.panel1.SuspendLayout();
             this.pnlTopArea.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.logDisplayBindingSource)).BeginInit();
@@ -35,10 +35,9 @@
             // MainContentPanel
             // 
             this.MainContentPanel.BackColor = System.Drawing.SystemColors.Menu;
-            this.MainContentPanel.Location = new System.Drawing.Point(12, 229);
-            this.MainContentPanel.Margin = new System.Windows.Forms.Padding(6);
+            this.MainContentPanel.Location = new System.Drawing.Point(6, 127);
             this.MainContentPanel.Name = "MainContentPanel";
-            this.MainContentPanel.Size = new System.Drawing.Size(2140, 576);
+            this.MainContentPanel.Size = new System.Drawing.Size(1070, 300);
             this.MainContentPanel.TabIndex = 0;
             // 
             // panel1
@@ -46,18 +45,16 @@
             this.panel1.Controls.Add(this.btnPeople);
             this.panel1.Controls.Add(this.btnBooks);
             this.panel1.Controls.Add(this.btnLendings);
-            this.panel1.Location = new System.Drawing.Point(8, 25);
-            this.panel1.Margin = new System.Windows.Forms.Padding(6);
+            this.panel1.Location = new System.Drawing.Point(4, 13);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(268, 192);
+            this.panel1.Size = new System.Drawing.Size(134, 100);
             this.panel1.TabIndex = 1;
             // 
             // btnPeople
             // 
-            this.btnPeople.Location = new System.Drawing.Point(16, 125);
-            this.btnPeople.Margin = new System.Windows.Forms.Padding(6);
+            this.btnPeople.Location = new System.Drawing.Point(8, 65);
             this.btnPeople.Name = "btnPeople";
-            this.btnPeople.Size = new System.Drawing.Size(236, 44);
+            this.btnPeople.Size = new System.Drawing.Size(118, 23);
             this.btnPeople.TabIndex = 2;
             this.btnPeople.Text = "People";
             this.btnPeople.UseVisualStyleBackColor = true;
@@ -65,10 +62,9 @@
             // 
             // btnBooks
             // 
-            this.btnBooks.Location = new System.Drawing.Point(16, 69);
-            this.btnBooks.Margin = new System.Windows.Forms.Padding(6);
+            this.btnBooks.Location = new System.Drawing.Point(8, 36);
             this.btnBooks.Name = "btnBooks";
-            this.btnBooks.Size = new System.Drawing.Size(236, 44);
+            this.btnBooks.Size = new System.Drawing.Size(118, 23);
             this.btnBooks.TabIndex = 1;
             this.btnBooks.Text = "Books";
             this.btnBooks.UseVisualStyleBackColor = true;
@@ -76,10 +72,9 @@
             // 
             // btnLendings
             // 
-            this.btnLendings.Location = new System.Drawing.Point(16, 19);
-            this.btnLendings.Margin = new System.Windows.Forms.Padding(6);
+            this.btnLendings.Location = new System.Drawing.Point(8, 10);
             this.btnLendings.Name = "btnLendings";
-            this.btnLendings.Size = new System.Drawing.Size(236, 44);
+            this.btnLendings.Size = new System.Drawing.Size(118, 23);
             this.btnLendings.TabIndex = 0;
             this.btnLendings.Text = "LendingsViewModel";
             this.btnLendings.UseVisualStyleBackColor = true;
@@ -88,19 +83,16 @@
             // lstMainViewLog
             // 
             this.lstMainViewLog.FormattingEnabled = true;
-            this.lstMainViewLog.ItemHeight = 25;
-            this.lstMainViewLog.Location = new System.Drawing.Point(12, 824);
-            this.lstMainViewLog.Margin = new System.Windows.Forms.Padding(6);
+            this.lstMainViewLog.Location = new System.Drawing.Point(6, 431);
             this.lstMainViewLog.Name = "lstMainViewLog";
-            this.lstMainViewLog.Size = new System.Drawing.Size(2134, 179);
+            this.lstMainViewLog.Size = new System.Drawing.Size(1069, 95);
             this.lstMainViewLog.TabIndex = 2;
             // 
             // btnClose
             // 
-            this.btnClose.Location = new System.Drawing.Point(2002, 1018);
-            this.btnClose.Margin = new System.Windows.Forms.Padding(6);
+            this.btnClose.Location = new System.Drawing.Point(1001, 529);
             this.btnClose.Name = "btnClose";
-            this.btnClose.Size = new System.Drawing.Size(150, 44);
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
             this.btnClose.TabIndex = 4;
             this.btnClose.Text = "&Close App";
             this.btnClose.UseVisualStyleBackColor = true;
@@ -109,36 +101,34 @@
             // pnlTopArea
             // 
             this.pnlTopArea.Controls.Add(this.lendingsSearchCriteriaControl1);
-            this.pnlTopArea.Location = new System.Drawing.Point(288, 25);
-            this.pnlTopArea.Margin = new System.Windows.Forms.Padding(6);
+            this.pnlTopArea.Location = new System.Drawing.Point(144, 13);
             this.pnlTopArea.Name = "pnlTopArea";
-            this.pnlTopArea.Size = new System.Drawing.Size(1862, 192);
+            this.pnlTopArea.Size = new System.Drawing.Size(931, 109);
             this.pnlTopArea.TabIndex = 5;
-            // 
-            // lendingsSearchCriteriaControl1
-            // 
-            this.lendingsSearchCriteriaControl1.Location = new System.Drawing.Point(838, 10);
-            this.lendingsSearchCriteriaControl1.Margin = new System.Windows.Forms.Padding(12);
-            this.lendingsSearchCriteriaControl1.Name = "lendingsSearchCriteriaControl1";
-            this.lendingsSearchCriteriaControl1.SearchLendingsViewModel = null;
-            this.lendingsSearchCriteriaControl1.Size = new System.Drawing.Size(1000, 173);
-            this.lendingsSearchCriteriaControl1.TabIndex = 0;
             // 
             // logDisplayBindingSource
             // 
             this.logDisplayBindingSource.DataSource = typeof(KesselRun.HomeLibrary.UiModel.ViewModels.MainViewModel);
             // 
+            // lendingsSearchCriteriaControl1
+            // 
+            this.lendingsSearchCriteriaControl1.Location = new System.Drawing.Point(419, 5);
+            this.lendingsSearchCriteriaControl1.Margin = new System.Windows.Forms.Padding(6);
+            this.lendingsSearchCriteriaControl1.Name = "lendingsSearchCriteriaControl1";
+            this.lendingsSearchCriteriaControl1.SearchLendingsViewModel = null;
+            this.lendingsSearchCriteriaControl1.Size = new System.Drawing.Size(500, 98);
+            this.lendingsSearchCriteriaControl1.TabIndex = 0;
+            // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(2174, 1079);
+            this.ClientSize = new System.Drawing.Size(1087, 554);
             this.Controls.Add(this.pnlTopArea);
             this.Controls.Add(this.btnClose);
             this.Controls.Add(this.lstMainViewLog);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.MainContentPanel);
-            this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "MainForm";
             this.Text = "Home Library";
             this.panel1.ResumeLayout(false);
